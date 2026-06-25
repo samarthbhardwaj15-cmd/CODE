23273a465ea5a19355e567ff34ff89c1b140e5c1
SAMARTH <samarth.bhardwaj15@gmail.com>
2026-06-25 04:41:46 +0000

    initial commit

--- import random.py
+++ import random.py
@@ -1,23 +1,21 @@
-import random
-import string
+import requests
 
-length = int(input("Enter paassword length (minimum 4): "))
+API_KEY = "YOUR_API_KEY"
 
-if length < 4:
-    print("password length must be at least 4")
+city = input("Enter City Name: ")
+
+url =  f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+
+response = requests.get(url)
+data = response.json()
+
+if response.status_code == 200:
+    print("\nWeather Information")
+    print("---------------------")
+    print("City:", data["name"])
+    print("Temp:", data["main"]["temp"], "c")
+    print("Humadity:", data["main"]["humadity"], "%")
+    print("Wind Speed:", data["wind"]["speed"], "m/s")
+    print("condition:", data["weather"][0]["description"])
 else:
-    password = [
-        random.choice(string.ascii_uppercase),
-        random.choice(string.ascii_lowercase),
-        random.choice(string.digits),
-        random.choice(string.punctuation)
-    ]    
-
-    characters = string.ascii_letters + string.digits + string.punctuation
-
-    password += [random.choice(characters) for _ in range(length - 4)]
-
-    random.shuffle(password)
-
-    print("Strong Password:", "".join(password))
-    
+    print("City Not Found!")    
