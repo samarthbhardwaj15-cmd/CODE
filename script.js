b3d1cad32640f4f8ea9cef89d1baf6f5975cba1d
SAMARTH <samarth.bhardwaj15@gmail.com>
2026-06-26 11:44:36 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,66 @@
+// let isAdmin = true;
+// let isLoggedIn = false;
+
+// if (isAdmin || isLoggedIn) {
+    // console.log("Access granted");
+//} else {
+   // console.log("Access denied");
+//} 
+
+
+//function rps(user, computer) {
+    //if (user === computer) return "draw";
+
+    //if (user ==="rock" && computer === "scissor") return "user";
+    //if (user ==="scissor" && computer === "paper") return "user";
+    //if (user ==="paper" && computer === "rock") return "user";
+
+    //return "computer";
+//}    
+
+//console.log(rps("rock", "paper"));
+
+//for(let i = 1; i<20; i++) {
+    //if (i % 2 === 0) {
+        //console.log(i);
+  //}
+//}
+
+//let i = 1;
+//while(i<16){
+    //if(i%2 === 1){
+        //console.log(i);
+    //}
+    //i++;
+//}   
+
+//for(let i = 1; i<10; i++){
+    //console.log(`5 * ${i} = ${5 * i}`);
+//}
+
+//let sum = 0;
+//for(let i = 1; i<5; i++){
+    //sum = sum + i;
+    //console.log(sum);
+//}
+
+//for(let i=1; i<10; i++){
+    //if(i%3 === 0)
+        //console.log(i);
+//}
+
+//let val = prompt("give a nu:");
+
+//for (let i = 1; i <= val; i++) {
+    //if (i % 2 === 0) {
+        //console.log(`${i} is even`);
+    //} else {
+        //console.log(`${i} is odd`)
+    //}
+//}
+
+for(let i = 1; i<10; i++){
+    if(i % 3 & i % 5 === 0){
+        console.log(i)
+    }
+}
