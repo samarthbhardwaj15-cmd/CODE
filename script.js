12c0f4fe2d4d56fd1a4b06401f8ef018a3f31717
SAMARTH <samarth.bhardwaj15@gmail.com>
2026-06-28 08:18:51 +0000

    Initial commit

--- script.js
+++ script.js
@@ -59,8 +59,10 @@
     //}
 //}
 
-for(let i = 1; i<10; i++){
-    if(i % 3 & i % 5 === 0){
-        console.log(i)
-    }
-}
+//for(let i = 1; i<10; i++){
+    //if(i % 3 & i % 5 === 0){
+        //console.log(i)
+    //}
+//}
+
+  
