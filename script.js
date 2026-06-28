3e8f5bfbf4f775c31d49302d9a578698e3f682b8
SAMARTH <samarth.bhardwaj15@gmail.com>
2026-06-28 08:39:51 +0000

    Initial commit

--- script.js
+++ script.js
@@ -64,5 +64,88 @@
         //console.log(i)
     //}
 //}
+//for(let i = 1; i<10; i++){
+    //if(i%7 === 0){
+        //break;
+    //}
+    //console.log(i)
+//}
 
+//let count = 0;
+//for (let i = 1; i < 10; i++) {
+    //if (i % 2 === 1) {
+        //count++;
+        //console.log(i);
+    //}  
+
+    //if (count === 5) break;
+//}
+
+//let fnc = function() {
+    //console.log("hi");
+//}
+//fnc();
+
+//function abcd() {
+    //return 12;
+//}
+
+
+//let val = abcd(23);
+//console.log(val);
+
+//let a = 12;
+
+// function abcd() {
+    //console.log("hehehe");
+//}
+
+//function hui(){
+    //a++;
+ //}
+//console.log("hui");
+//greet();
+
+//function greet() {
+    //console.log("hello!");
+//}
+
+//function welcome(name) {
+    //console.log(name);
+//}    
+//welcome("harsh");
+
+//function sayHi(name = "Guest") {
+    //console.log("Hi", name)
+//}
+//sayHi();
+
+//function abcd(a,b,c, ...val) {
+    //console.log(val);
+//}
+//abcd(1, 2, 3, 4, 5, 6);
+
+//function checkAge(age) {
+    //if (age < 18) return "Too young";
+    //return "Allowed";
+//}
+
+//console.log(checkAge(12));
+
+//function outer() {
+    //let count = 0;
+    //return function() {
+        //count++;
+        //console.log(count);
+    //};
+//}
+//const counter = outer();
+//counter();
+//counter();
+
+function bmi(weight, height) {
+    return weight / (height * height);
+}
+
+console.log(bmi(69, 1.7). toFixed(2));  
   
