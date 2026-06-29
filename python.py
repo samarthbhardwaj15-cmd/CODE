22206136931e991fe6a929eb52e6949fd86efbde
Samarth Bhardwaj <samarth.bhardwaj15@gmail.com>
2026-06-29 09:20:36 -0700

    Initial commit

--- python.py
+++ python.py
@@ -1,19 +1,58 @@
 print("Hello World!")
 #calculater
 num1 = float(input("Enter first number:"))
-op =  input("Enter operator"(+,-,*,/):")
+op =  input("Enter operator (+,-,*,/):")
 num2 = float(input("Enter second number:"))
 
 if op == "+":
     print(num1 + num2)
 elif op == "-":
-     print(mum1 - num2)
+     print(num1 - num2)
 elif op == "*":
-   print(num1)* num2)
+     print(num1 * num2)
 elif op == "/":
    if num2 != 0:
        print("Answer =", num1 / num2)
    else:
-       print("coannot divide by zero")
+       print("Cannot divide by zero")
 else:
-    print("Invalid operater")       
+    print("Invalid operator")       
+
+#Even or Odd
+num = int(input("Enter a number:"))
+if num % 2 == 0:
+    print(num, "is an evne number")
+else:
+    print(num, "is an odd number")
+    
+#largest of three numbers
+a = float(input("Enter first number:"))
+b = float(input("Enter second number:"))
+c = float(input("Enter third number:"))
+
+if a >= b and a >= c:
+    print(a, "is the largest number")
+elif b >= a and b >= c:
+    print(b, "is the largest number")
+else:
+    print(c, "is the largest number")
+    
+#Number Guessing Game
+import random
+print("welcome to the number guessing game!")
+print("guess a number between 1 and 10")
+
+secret = random.randint(1,10)
+attempts = 0
+
+while True:
+    guess = int(input("Enter your guess:"))
+    attempts += 1
+    
+    if guess < secret_number:
+        print("Too low! Try again:")
+    elif guess > secret_number:
+        print("too high ! try again:")
+    else:
+        print("Congratulations! you guessed the number in", attempts, "attempts.")
+        break
