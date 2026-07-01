371098ecc0d73133a3e2d9e407349296fead4940
Samarth Bhardwaj <samarth.bhardwaj15@gmail.com>
2026-06-30 22:19:42 -0700

    Initial commit

--- DSA.js
+++ DSA.js
@@ -2,10 +2,14 @@
 console.log("1"+1);
 console.log("1"-1);
 
+
+//Accept and print the answer
 let age = prompt("What is your age?"); 
 age = Number(age); 
 console.log(typeof age);
 
+
+//swap two numbers without using a third variable
 let a = 10;
 let b = 20;
 let c;
@@ -17,11 +21,27 @@ b = c;
 console.log(a);
 console.log(b);
 
+//swap two variable
+
 let x = 5;
 let y = 10;
 
-a = a + b;
-b = a - b;
-a = a - b;
+x = x + y;
+y = x - y;
+x = x - y;
 
-console.log(a, b); 
+console.log(x, y); 
+
+let w = 10;
+let e = 20;
+
+[w, e] = [e, w];
+
+console.log(w, e);
+
+
+//num
+let z = 1234;
+console.log(Math.floor(z/10))
+console.log(Math.floor(z * 10))
+
